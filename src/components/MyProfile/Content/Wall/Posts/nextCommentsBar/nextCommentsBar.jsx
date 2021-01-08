55f1a8d5e02a35d15033c11b630ae789ec5eca7b
WeAreGeronimo <grantero16@gmail.com>
2021-01-08 05:54:03 +0300

    add button "load next comments" and logic for them

--- src/components/MyProfile/Content/Wall/Posts/nextCommentsBar/nextCommentsBar.jsx
+++ src/components/MyProfile/Content/Wall/Posts/nextCommentsBar/nextCommentsBar.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import _css from './nextCommentsBar.module.css';
+import AllCommentsAreShowed from './allCommentsAreShowed';
 
 const NextCommentsBar = (props) => {
+  if (props.commentsCountInApi <= 3) {
+    props.setAllCommentsAreShowed(true);
+  }
   return (
     <div>
-      <div
-        className={_css.nextBlock}
-        onClick={() => {
-          props.getNextWallPosts(props.profileId, props.nextPosts);
-          if (props.nextPosts + 1 < props.countOfLoads) {
-            props.setNextPosts(props.nextPosts + 1);
-          } else if (props.nextPosts + 1 === props.countOfLoads) {
-            props.setAllPostAreShowed(true);
-          }
-        }}
-      >
-        <span>Показать следующие комментарии</span>
-      </div>
+      {!props.allCommentsAreShowed && (
+        <div
+          className={_css.nextBlock}
+          onClick={() => {
+            props.getNextPostsComments(
+              props.postId,
+              props.nextComments,
+            );
+            if (props.nextComments + 1 < props.countOfLoadsCom) {
+              props.setNextComments(props.nextComments + 1);
+            } else if (
+              props.nextComments + 1 ===
+              props.countOfLoadsCom
+            ) {
+              props.setAllCommentsAreShowed(true);
+            }
+          }}
+        >
+          <span>Загрузить еще...</span>
+        </div>
+      )}
+      {/* {props.allCommentsAreShowed && <AllCommentsAreShowed />} */}
     </div>
   );
 };
