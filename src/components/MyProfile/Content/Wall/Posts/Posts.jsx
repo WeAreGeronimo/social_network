991a93664137aaecb8f992536030a8fee4cd1199
WeAreGeronimo <grantero16@gmail.com>
2021-01-08 06:01:13 +0300

    added protect window before deleting comments, and styles for them

--- src/components/MyProfile/Content/Wall/Posts/Posts.jsx
+++ src/components/MyProfile/Content/Wall/Posts/Posts.jsx
@@ -3,9 +3,12 @@ import { Field, reduxForm } from 'redux-form';
 import _css from './Posts.module.css';
 import Like from '../../../../../assets/Like/Like';
 import CommentSvg from '../../../../../assets/CommentImageSvg/CommentImageSvg';
-import CommentsItem from '../Comments/Comments';
+import CommentsItem from './Comments/Comments';
 import { beautifulWhenTimeText } from '../../../../../common/TimeTextFunc';
 import NextCommentsBar from './nextCommentsBar/nextCommentsBar';
+import NextPostsBar from '../nextPostsBar/nextPostsBar';
+import TrashCan from '../../../../../assets/trashCan/TrashCan';
+import DeleteButton from '../../../../../assets/DeleteImageSvg/Delete';
 
 const FormForComments = (props) => {
   return (
@@ -31,7 +34,6 @@ const POST = (props) => {
   const ComentsReduxForm = reduxForm({ form: 'CommentsForm' })(
     FormForComments,
   );
-  console.log(props.commentsCountInApi);
   const initialTimeValue = beautifulWhenTimeText(props.whenTime);
   const [time, setTime] = useState(initialTimeValue);
   const oldTimerForPost = useRef(null);
@@ -53,26 +55,93 @@ const POST = (props) => {
       props.postId,
     );
   };
-  const [Boolean, setBoolean] = useState(
-    props.likes.includes(props.AuthUserId),
-  );
-
   const [sendCommentMode, setSendCommentMode] = useState(false);
-
   const LikesToggleComments = (value) => {
     return props.ToggleLikeComment(value);
   };
-  return (
+  const [deleteShowPost, setDeleteShowPost] = useState(
+    _css.deleteBlockNone,
+  );
+  const countUnloadedComments = props.commentsCountInApi - 3;
+  const countOfLoadsCom = Math.ceil(countUnloadedComments / 3);
+  let userHaveASomeComments = false;
+  const [allCommentsAreShowed, setAllCommentsAreShowed] = useState(
+    false,
+  );
+  const [deleteModePost, setDeleteModePost] = useState(false);
+  const deletePost = () => {
+    props.deletePost(props.postId);
+    setDeleteModePost(false);
+  };
+  const [nextComments, setNextComments] = useState(0);
+  if (props.commentsCountInApi !== 0) {
+    userHaveASomeComments = true;
+  }
+  props.comments.sort(function (a, b) {
+    return a.commentId - b.commentId;
+  });
+  let itsMyWall = false;
+  let itsYourPost = false;
+  if (props.AuthUserId === props.whoseWall) {
+    itsMyWall = true;
+  }
+  if (props.AuthUserId === props.from) {
+    itsYourPost = true;
+  }
+  let checkRightsForDeletingPost = false;
+  if (itsMyWall || itsYourPost) {
+    checkRightsForDeletingPost = true;
+  }
+
+  return deleteModePost ? (
+    <div className={_css.wrap_delete}>
+      <span className={_css.deleteMessage}>
+        Пост будет безвозвратно удален.
+      </span>
+      <div className={_css.deleteBlock}>
+        <div className={_css.cancelBlock_button}>
+          <span
+            onClick={() => {
+              setDeleteModePost(false);
+            }}
+          >
+            Отменить
+          </span>
+        </div>
+        <div className={_css.deleteBlock_button}>
+          <span onClick={deletePost}>Удалить</span>
+        </div>
+      </div>{' '}
+    </div>
+  ) : (
     <div className={_css.Post}>
       <div className={_css.avatar}>
         <img src="https://pm1.narvii.com/6889/74979d4d2744ec6e27995b6e866f091d04c0b40cr1-515-414v2_uhq.jpg" />
       </div>
-      <div className={_css.name_time_area}>
+      <div
+        className={_css.name_time_area}
+        onPointerEnter={(e) => {
+          setDeleteShowPost(_css.deleteBlockDisplay);
+        }}
+        onPointerLeave={(e) => {
+          setDeleteShowPost(_css.deleteBlockNone);
+        }}
+      >
         <div className={_css.name_surname}>
           <a href="#">
             {props.name} {props.nickname} {props.surname}{' '}
           </a>{' '}
           написал:
+        </div>
+        <div className={_css.deleteDiv}>
+          {checkRightsForDeletingPost && (
+            <span
+              className={deleteShowPost}
+              onClick={() => setDeleteModePost(true)}
+            >
+              <DeleteButton />
+            </span>
+          )}
         </div>
         <div className={_css.time}>{time}</div>
       </div>
@@ -103,7 +172,6 @@ const POST = (props) => {
               <span
                 role="button"
                 onClick={() => {
-                  setBoolean(!Boolean);
                   props.ToggleLike(props.postId);
                 }}
                 aria-label="Like Button"
@@ -123,8 +191,9 @@ const POST = (props) => {
       </div>
       <div className={_css.commentBlock}>
         {/* eslint-disable-next-line array-callback-return */}
-        {props.comments?.map((commentEl) => (
+        {props?.comments.map((commentEl) => (
           <CommentsItem
+            whoseWall={props.whoseWall}
             from={commentEl.from}
             AuthUserId={props.AuthUserId}
             postId={props.postId}
@@ -142,6 +211,21 @@ const POST = (props) => {
         {sendCommentMode ? (
           <ComentsReduxForm onSubmit={onSubmit} />
         ) : null}
+        {userHaveASomeComments && (
+          <NextCommentsBar
+            commentsCountInApi={props.commentsCountInApi}
+            allCommentsAreShowed={allCommentsAreShowed}
+            nextComments={nextComments}
+            countOfLoadsCom={countOfLoadsCom}
+            setNextComments={setNextComments}
+            setAllCommentsAreShowed={setAllCommentsAreShowed}
+            postId={props.postId}
+            getNextPostsComments={props.getNextPostsComments}
+          />
+        )}{' '}
+        {/* {!userHaveASomeComments && ( */}
+        {/*  <div>Пользователь не добавил ни одного поста...</div> */}
+        {/* )} */}
       </div>
     </div>
   );
